1fc18d9e5b43ef998e7030a68ae2ae6f5e3038a7
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2020-02-09 17:10:31 -0300

    Add css archives for components

--- src/components/Main/Feed/Post/index.tsx
+++ src/components/Main/Feed/Post/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './post.css';
 
 import PostHeader from './PostHeader';
 import PostContent from './PostContent';
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './sectionMain.css';
 
 import Feed from './Feed';
 
