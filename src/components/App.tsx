a071d35faffb335aa306b5b629fd72712c530ec3
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2020-02-09 16:27:00 -0300

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import './App.css';
 
 import Header from './Header';
-import Main from './Main';
 
 const App = () => {
   return (
     <div className="App">
       <section className="header">
         <Header />
-      </section>
-      <section>
-        <Main />
       </section>
     </div>
   );
